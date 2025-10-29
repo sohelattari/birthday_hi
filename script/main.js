b2189cbf4ad460d1bd028f9a552ec29954b6670f
mohammad sohel <soheldayer04@gmail.com>
2025-10-29 19:24:09 +0530

    add song again

--- script/main.js
+++ script/main.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
         cancelButtonText: 'No',
     }).then((result) => {
         if (result.isConfirmed) {
-            // document.querySelector('.song').play();
+            document.querySelector('.song').play();
             animationTimeline();
         } else {
             animationTimeline();
